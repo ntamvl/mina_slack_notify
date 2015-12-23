9ac1f7faa1dbcc00a0eb06cf0ea8b98e900787fd
Tam Nguyen <nguyentamvinhlong@gmail.com>
2015-12-23 17:29:48 +0700

    update text format

--- lib/mina_slack_notify/tasks.rb
+++ lib/mina_slack_notify/tasks.rb
@@ -94,7 +94,7 @@ namespace :slack do
 
   def send_message(params = {})
     slack_url = "https://#{slack_team_domain}.slack.com/services/hooks/slackbot?token=#{slack_api_token}&channel=%23#{params[:channel]}"
-    HTTParty.post(slack_url, {text: params[:text]})
+    HTTParty.post(slack_url, {body: params[:text]})
   end
 
 end
